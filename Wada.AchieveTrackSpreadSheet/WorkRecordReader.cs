02e0a004d33961ae537a377658fb401ffe62d174
Matazoro-Kemtari <k_hirano@wadass.com>
2023-06-20 15:02:36 +0900

    日報取得の正常系完成

---
+++ Wada.AchieveTrackSpreadSheet/WorkRecordReader.cs
@@ -0,0 +1,52 @@
+﻿using ClosedXML.Excel;
+using Wada.AchieveTrackService;
+using Wada.AchieveTrackService.AchieveTrackReader;
+
+namespace Wada.AchieveTrackSpreadSheet;
+
+public class WorkRecordReader : IWorkRecordReader
+{
+    public async Task<IEnumerable<WorkRecord>> ReadWorkRecordsAsync(Stream stream)
+    {
+        using var xlBook = new XLWorkbook(stream);
+        try
+        {
+            IXLWorksheet targetSheet = xlBook.Worksheets.First();
+            var usedRage = targetSheet.RangeUsed();
+
+            return await Task.WhenAll(
+                usedRage.Rows()
+                        // ヘッダ行を飛ばす
+                        .Skip(1)
+                        .Select(async row => await Task.Run(() =>
+                        {
+                            const string workingDateLetter = "A";
+                            const string employeeNumberLetter = "B";
+                            const string workingNumberLetter = "E";
+                            const string monHourLetter = "J";
+
+                            if (!row.Cell(workingDateLetter).TryGetValue(out DateTime workingDate))
+                                throw new DomainException(
+                                    $"作業日が取得できませんでした 行: {row.RowNumber()}");
+
+                            if (!row.Cell(employeeNumberLetter).TryGetValue(out int employeeNumber))
+                                throw new DomainException(
+                                    $"社員番号が取得できませんでした 行: {row.RowNumber()}");
+
+                            if (!row.Cell(workingNumberLetter).TryGetValue(out string workingNumber))
+                                throw new DomainException(
+                                    $"作業番号が取得できませんでした 行: {row.RowNumber()}");
+
+                            if (!row.Cell(monHourLetter).TryGetValue(out decimal manHour))
+                                throw new DomainException(
+                                    $"工数が取得できませんでした 行: {row.RowNumber()}");
+
+                            return WorkRecord.Create(workingDate, (uint)employeeNumber, workingNumber, manHour);
+                        })));
+        }
+        catch (InvalidOperationException ex)
+        {
+            throw new DomainException("ワークシートが見つかりません", ex);
+        }
+    }
+}
---
+++ Wada.AchieveTrackSpreadSheetTests/WorkRecordReaderTests.cs
@@ -0,0 +1,85 @@
+﻿using ClosedXML.Excel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Wada.AchieveTrackService;
+using Wada.AchieveTrackService.AchieveTrackReader;
+
+namespace Wada.AchieveTrackSpreadSheet.Tests
+{
+    [TestClass()]
+    public class WorkRecordReaderTests
+    {
+        [TestMethod()]
+        public async Task 正常系_日報が取得できること()
+        {
+            // given
+            using var workbook = MakeTestBook();
+            using var xlsStream = new MemoryStream();
+            workbook.SaveAs(xlsStream);
+
+            // when
+            IWorkRecordReader workRecordReader = new WorkRecordReader();
+            var actual = await workRecordReader.ReadWorkRecordsAsync(xlsStream);
+
+            // then
+            Assert.IsNotNull(actual);
+            var expected = MakeTestDatas();
+            CollectionAssert.AreEquivalent(expected.ToArray(), actual.ToArray());
+        }
+
+        private static IXLWorkbook MakeTestBook()
+        {
+            XLWorkbook workbook = new();
+            var sht = workbook.AddWorksheet();
+            MakeTestWorkRecordHeader().Select((header, i) => (header, i))
+                                      .ToList()
+                                      .ForEach(x =>
+                                      {
+                                          sht.Cell(1, x.i + 1).SetValue(x.header);
+                                      });
+
+            MakeTestDatas().Select((item, i) => (item, i))
+                           .ToList()
+                           .ForEach(x =>
+            {
+                sht.Cell(x.i + 2, "A").SetValue(x.item.WorkingDate);
+                sht.Cell(x.i + 2, "B").SetValue(x.item.EmployeeNumber);
+                sht.Cell(x.i + 2, "E").SetValue(x.item.WorkingNumber);
+                sht.Cell(x.i + 2, "J").SetValue(x.item.ManHour);
+            });
+            return workbook;
+        }
+
+        private static IEnumerable<WorkRecord> MakeTestDatas() => new List<WorkRecord>
+        {
+            TestWorkRecordFactory.Create(workingDate: new DateTime(2023,4,1), manHour: 0.1m),
+            TestWorkRecordFactory.Create(workingDate: new DateTime(2023,4,1), manHour: 3.4m),
+            TestWorkRecordFactory.Create(workingDate: new DateTime(2023,4,1), manHour: 2.5m),
+            TestWorkRecordFactory.Create(workingDate: new DateTime(2023,4,1), manHour: 4.0m),
+            TestWorkRecordFactory.Create(workingDate: new DateTime(2023,4,2), manHour: 0.5m),
+            TestWorkRecordFactory.Create(workingDate: new DateTime(2023,4,2), manHour: 3.5m),
+            TestWorkRecordFactory.Create(workingDate: new DateTime(2023,4,3), manHour: 5.5m),
+            TestWorkRecordFactory.Create(workingDate: new DateTime(2023,4,3), manHour: 2.0m),
+        };
+
+        private static string[] MakeTestWorkRecordHeader() => new string[]
+        {
+            "日付",
+            "社員番号",
+            "氏名",
+            "実働時間",
+            "作業番号",
+            "コード",
+            "作業名",
+            "特記事項",
+            "目標工数",
+            "工数",
+            "ヘッダ",
+            "番号",
+            "部署",
+            "機種名",
+            "直間区分",
+            "大分類",
+            "中分類",
+        };
+    }
+}
