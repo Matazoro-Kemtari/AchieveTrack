29c0e67181ca38897b2809496e3a93ae0f8b93f5
Matazoro-Kemtari <k_hirano@wadass.com>
2023-06-21 09:09:29 +0900

    WorkingNumber ValueObjectのテスト追加

---
+++ Wada.AchieveTrackServiceTests/AchieveTrackReader/WorkRecordTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Wada.AchieveTrackService.AchieveTrackReader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wada.AchieveTrackService.AchieveTrackReader.Tests
+{
+    [TestClass()]
+    public class WorkRecordTests
+    {
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ Wada.AchieveTrackServiceTests/ValueObjects/WorkingNumberTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Wada.AchieveTrackService.ValueObjects;
+
+namespace Wada.AchieveTrackServiceTests.ValueObjects
+{
+    [TestClass()]
+    public class WorkingNumberTests
+    {
+        [DataTestMethod]
+        [DataRow("23A-1")]
+        [DataRow("X23A-1")]
+        public void 正常系_オブジェクト生成できること(string value)
+        {
+            // given
+            // when
+            var workingnumber = WorkingNumber.Create(value);
+
+            // then
+            Assert.IsNotNull(workingnumber);
+        }
+
+        [DataTestMethod]
+        [DataRow("Z1A-1")]
+        [DataRow("123A-1")]
+        [DataRow("1A-12345")]
+        [DataRow("1漢-1")]
+        [DataRow("1a-1")]
+        public void 異常系_形式不正な作業番号のときは例外を返すこと(string value)
+        {
+            // given
+            // when
+            void target() => _= WorkingNumber.Create(value);
+
+            // then
+            var ex = Assert.ThrowsException<Data.OrderManagement.Models.ValueObjects.WorkingNumberException>(target);
+            var message = $"正しい作業Noの形式を入力してください 値: {value}";
+            Assert.AreEqual(message, ex.Message);
+        }
+    }
+}
