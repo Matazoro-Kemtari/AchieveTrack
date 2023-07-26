3844b0f2ef974d155e0273a8062e4b2b4f9de468
Matazoro-Kemtari <k_hirano@wadass.com>
2023-07-26 17:18:18 +0900

    正常系 設計管理追加/参照テスト

--- Wada.AchievementEntry/ViewModels/MessageNotificationViaLivet.cs
+++ Wada.AchievementEntry/ViewModels/MessageNotificationViaLivet.cs
@@ -8,19 +8,19 @@ namespace Wada.AchievementEntry.ViewModels;
 public class MessageNotificationViaLivet
 {
     [Logging]
-    public static InformationMessage MakeErrorMessage(string message, string title = "実績登録") => new(
+    public static InformationMessage MakeErrorMessage(string message, string title = "受注管理実績登録") => new(
         message, title, MessageBoxImage.Error, "Info");
 
     [Logging]
-    public static InformationMessage MakeExclamationMessage(string message, string title = "実績登録") => new(
+    public static InformationMessage MakeExclamationMessage(string message, string title = "受注管理実績登録") => new(
         message, title, MessageBoxImage.Exclamation, "Info");
 
     [Logging]
-    public static InformationMessage MakeInformationMessage(string message, string title = "実績登録") => new(
+    public static InformationMessage MakeInformationMessage(string message, string title = "受注管理実績登録") => new(
         message, title, MessageBoxImage.Information, "Info");
 
     [Logging]
-    public static ConfirmationMessage MakeQuestionMessage(string message, string title = "実績登録") => new(
+    public static ConfirmationMessage MakeQuestionMessage(string message, string title = "受注管理実績登録") => new(
         message, title, MessageBoxImage.Question, "Confirm")
     { Button = MessageBoxButton.YesNo };
 
